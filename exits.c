9d28c094d30029fbf69b3975e95f2be953da7e83
michael1haile <haile12black12@gmail.com>
2023-04-26 11:23:31 -0700

    update

--- exits.c
+++ exits.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "simple_shell.h"
 
 /**
  **_strncpy - copy a string
--- string.c
+++ string.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "simple_shell.h"
 
 /**
  * _strlen - returns length of a string
