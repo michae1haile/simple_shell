cad762dbb0144ef127b9921d3f627d41d241dace
belayaa <belayaa2013@gmail.com>
2023-04-26 12:02:04 -0700

    getenvironment

---
+++ getenvi.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * get_environ - returns the string array copy of our environ
+ * @info:  arguments
+ * Return: 0(success)
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _rmenv - Remove an environment variable
+ * @info:  arguments.
+ * @var: string env variable ppty
+ * Return: 1 on delete, 0 otherwise
+ */
+int _rmenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t j = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), j);
+			j = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		j++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable
+ * @info: arguments.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buff = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buff = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buff, var);
+	_strcat(buff, "=");
+	_strcat(buff, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buff;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buff, 0);
+	free(buff);
+	info->env_changed = 1;
+	return (0);
+}
+
