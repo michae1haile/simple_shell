1951d4344377618e83da9d6af9a2f22fb6a34ea6
belayaa <belayaa2013@gmail.com>
2023-04-27 13:19:04 -0700

    shell

--- shell_string.c
+++ shell_string.c
@@ -1,87 +1,80 @@
 #include "simple_shell.h"
 /**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+        int n = 0;
+
+        if (!str)
+                return;
+        while (str[n] != '\0')
+        {
+                _putchar(str[n]);
+                n++;
+        }
+}
+/**
  * _strcpy - copies a string
  * @dest: the destination
  * @src: the source
- *
  * Return: pointer to destination
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int n = 0;
 
 	if (dest == src || src == 0)
 		return (dest);
-	while (src[i])
+	while (src[n])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[n] = src[n];
+		n++;
 	}
-	dest[i] = 0;
+	dest[n] = 0;
 	return (dest);
 }
-
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
- *
  * Return: pointer to the duplicated string
  */
 char *_strdup(const char *str)
 {
-	int length = 0;
-	char *ret;
+	int len = 0;
+	char *ptr;
 
 	if (str == NULL)
 		return (NULL);
 	while (*str++)
-		length++;
-	ret = malloc(sizeof(char) * (length + 1));
-	if (!ret)
+		len++;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
 		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
+	for (len++; len--;)
+		ptr[len] = *--str;
+	return (ptr);
 }
-
-/**
- * _puts - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
-void _puts(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	static int i;
+	static int n;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || n >= WRITE_BUF_SIZE)
 	{
-		write(1, buf, i);
-		i = 0;
+		write(1, buf, n);
+		n = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[n++] = c;
 	return (1);
 }
 
